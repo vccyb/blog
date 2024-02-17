4934c65a582f6700126ad26c965b7f2fdc728432
vccyb <13170906656@163.com>
2024-02-17 20:19:36 +0800

    add vercel analytics

--- astro.config.ts
+++ astro.config.ts
@@ -5,6 +5,7 @@ import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
 import { SITE } from "./src/config";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
@@ -37,4 +38,10 @@ export default defineConfig({
     },
   },
   scopedStyleStrategy: "where",
+  output: "server",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
